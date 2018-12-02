a6b68b334bd963eb0dc5e3b7500029fab2fb1369
Maurii19 <mdiazar17dw@ikzubirimanteo.com>
2018-12-02 16:52:56 +0100

    Municipos - Codigo Postal (Funciona)

--- js/validarFormulario.js
+++ js/validarFormulario.js
@@ -1,15 +1,19 @@
 $(document).ready(function () {
-  $('#formulario').validate({ // initialize the plugin
+  $('#formulario').validate({
     rules: {
       nif:{
+        required: true,
         minlength: 8,
-        maxlength: 8
+        maxlength: 8,
+        number: true
       },
       nombre: {
-        required: true
+        required: true,
+        minlength: 3
       },
       apellidos:{
-        required: true
+        required: true,
+        minlength: 3
       },
       sexo:{
         required: true
@@ -19,13 +23,18 @@ $(document).ready(function () {
         email: true
       },
       telefono:{
-        minlength: 8,
-        maxlength: 9,
-        digits: true
+        minlength: 9,
+        number: true
       },
+
       cp:{
         required: true,
-        digits: true
+        number: true,
+        maxlength: 5,
+        minlength: 5
+      },
+      municipio: {
+        required: true
       }
     },
 
@@ -43,5 +52,40 @@ $(document).ready(function () {
 
 });
 
+ function municipioFunction(){
+        if(document.getElementById("cp") != null && document.getElementById("cp").value.length == 5){
+          document.getElementById("municipio").disabled = false;
+        }else{
+          document.getElementById("municipio").disabled = true;
+        }
+      }
+
+$(document).ready(function(){
+      $("#cp").keyup(function(){
+        let cp = $("#cp").val();
+        switch (cp) {
+          case '20720':
+            $("#municipio").val("Azkoitia");
+            break;
+          case '20730':
+            $("#municipio").val("Azpeitia");
+            break;
+          case '20800':
+            $("#municipio").val("Zarautz");
+            break;
+          case '20100':
+            $("#municipio").val("Renteria");
+            break;
+          case '20750':
+            $("#municipio").val("Zumaia");
+            break;
+          case '20700':
+            $("#municipio").val("Zumarraga");
+            break;
+          default:
+            $("#municipio").val("Otros");
+        }
+      });
+    });
 
 
